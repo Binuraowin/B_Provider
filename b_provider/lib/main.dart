ce24fdb75e18fa3e3a5a22c5d30b6f4bfb5449c0
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-12-29 10:37:49 +0530

    Update main.dart

--- b_provider/lib/main.dart
+++ b_provider/lib/main.dart
@@ -2,11 +2,15 @@ import 'package:b_provider/screens/main_screens/category_mainPage.dart';
 import 'package:b_provider/screens/main_screens/map.dart';
 import 'package:b_provider/screens/main_screens/mapsnext.dart';
 import 'package:b_provider/screens/main_screens/nav_screens.dart';
+import 'package:b_provider/services/auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:b_provider/screens/wrapper.dart';
 import 'package:b_provider/screens/widgets/category.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
+import 'models/authUser.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,9 +22,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      home: NavScreen(),
+    return StreamProvider<AuthUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
